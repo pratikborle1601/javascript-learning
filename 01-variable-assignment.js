01dd891f9cbad8104230c2afbd7e6cda2e9d42af
pratikborle1601 <pratikborle1601@gmail.com>
2024-10-23 15:16:35 +0530

    Variable Declaration
    Variable Declaration

---
+++ 01-variable-assignment.js
@@ -0,0 +1,33 @@
+// Variable Declaration
+var city; 
+console.log("city: ", city); // undefined
+city = "malkapur"; // Variable Initialization
+
+console.log("city value: ", city); 
+// Variable Declaration
+var bestfriend;
+console.log("bestfriend:", bestfriend);// undefined
+
+bestfriend ="Milind Bhangale";// Variable Initialization
+
+console.log("bestfriend", bestfriend);
+// Variable Declaration
+var countryname;
+console.log("countryname",countryname);// undefined
+
+
+countryname= "India";// Variable Initialization
+console.log("countryname",countryname);
+// Variable Declaration
+var schoolname;
+console.log("schoolname",schoolname);// undefined
+
+
+schoolname= "Yashwant vidyalaya Wadji";// Variable Initialization
+console.log("schoolname",schoolname);
+// Variable Declaration
+var collagename;
+console.log("collagename",collagename);// undefined
+collagename= "Vidnyan Mahavidyalay Malkapur";// Variable Initialization
+console.log("collagename",collagename);
+
---
+++ 01-variableAssign.js
@@ -0,0 +1,46 @@
+// Variable Declaration
+var myFullName;   // undefined
+console.log("FullName:",myFullName);
+
+myFullName="Pratik Borle";  //   Variable Initialization
+console.log(" FullName:",myFullName);
+  
+ myFullName="Pratik Wasuedo Borle";  //variable Update
+console.log("FullName:",myFullName); 
+
+var myLove=" I love only JS";//   Variable Declare Initialization
+console.log("MyLove:",myLove);
+
+var firstName="Pratik";
+console.log("FirstName:",firstName);
+
+
+var lastName="Borle";
+console.log("LastName:",lastName);
+
+var age=23;
+console.log("Age:",age);
+
+var address=" At-post:Malegaon Dist:Buldana";
+console.log("Address:",address);
+
+var pinCode=4431011;
+console.log("PinCode:",pinCode);
+
+var gender="Male";
+console.log("Gender:",gender);
+
+var isMarried="false";
+console.log("isMarried:",isMarried);
+
+
+
+
+
+
+
+
+
+
+
+
