75891f4fbb3e9ec6a7646b342ba35bab24570e62
pratikborle1601 <pratikborle1601@gmail.com>
2024-10-25 16:15:25 +0530

    function.js
    function

---
+++ 03-funAssign01.js
@@ -0,0 +1,45 @@
+
+
+function hello(){
+    console.log( "hello brother!");
+    console.log("----------------");
+    
+    
+}
+hello()
+function my(){
+    console.log("i like a cricket");
+    console.log("--------------");
+    
+    
+}
+my()
+
+function personalDetails(firstName,lastName,collageName){
+    var firstName="Pratik"
+    console.log("firstName",firstName);
+    var lastName="Borle"
+    console.log("lastName",lastName);
+    var collageName="VMV Malkapur"
+    console.log("collageName",collageName);
+    
+    console.log("-----------------------------");
+}
+
+
+personalDetails()
+
+function addThreeValues( n1, n2, n3){
+    console.log("the values are:", n1, n2, n3);
+    var result= n1 + n2 + n3
+    return result;
+    
+
+}
+ var resultValue = addThreeValues (10.23, 600,40)
+ console.log("Addition is:",resultValue);
+ console.log("**********************");
+ var resultValue = addThreeValues ("Hello","Good", "Morning")
+ console.log("Addition is:",resultValue);
+ 
+ 
