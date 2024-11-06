cc434de3e090999a5582561bef057e39eced0360
pratikborle1601 <pratikborle1601@gmail.com>
2024-11-06 19:29:45 +0530

    for loop  statement
    for loop statement

---
+++ 06-for-assignment01.js
@@ -0,0 +1,60 @@
+
+console.log( "Print no from 5 to 15 by incrementing 1");
+
+for (let index = 5; index <=15; index++) {
+    console.log(index);
+    
+    
+}
+
+
+console.log("Print no from 50 to 40 by decrementing 1");
+
+for (let index = 50; index >=40; index--) {
+    console.log(index);
+    
+    
+}
+
+console.log(" Find first 15 odd number");
+
+
+for (let index = 1; index <30; index= index+2) {
+    console.log(index);
+    
+    
+}
+
+console.log("FIND FIRST 10 EVEN NUMBER");
+ 
+
+for (let index = 0; index <20; index= index+2) {
+    console.log(index);
+    
+    
+}
+  
+console.log("Print tabel of 5");
+
+for (let index = 5; index <=50; index= index+5) {
+    console.log(index);
+    
+    
+}
+
+
+console.log("Print tabel of 10");
+
+for (let index = 10; index <=100; index= index+10) {
+    console.log(index);
+    
+    
+}
+
+console.log("Print tabel of 10  in reverse order");
+
+for (let index = 100; index >=10; index= index-10) {
+    console.log(index);
+    
+    
+}
